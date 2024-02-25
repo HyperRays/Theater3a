51cfb766171013a6a69fdab031489155d69fed8f
servrcube <63092977+servrcube@users.noreply.github.com>
2024-02-25 20:56:09 +0100

    Updated

--- script.js
+++ script.js
@@ -19,140 +19,130 @@ $(".link-div").on("click", function () {
   return false;
 });
 
-let word_list = "Und Dann gab's Keines Mehr".split(" ");
+$(document).ready(() => {
+  // Story anim
 
-let delayFn = (val) => {
-  return (Math.log(val) * 2 + 1) * (Math.log(val) * 3.5 + 1) * 100;
-};
+  let limiter = (val, fn) => {
+    return Math.min(1, Math.max(0, fn(val)));
+  };
 
-word_list.forEach((word, i) => {
-  $(".opener>.text-container").append(
-    "<span class='initial-state word-" + i + "'>" + word + "</span>"
-  );
-  $(".opener>.text-container").append(" ");
-});
+  let show_text = () => {
+    let opacity_fn = (val) => {
+      return val;
+    };
 
-word_list.forEach((_, i) => {
+    let css_change = (elem, win, invert = false) => {
+      rel_height = $(elem).height() / $(window).height();
+
+      p20 = $(window).height() * 0.2;
+      const shift = 100;
+
+      let move_amount = -shift * (limiter(win, opacity_fn) - 1);
+      elem.css(
+        "transform",
+        "translate(" + move_amount * (!invert ? 1 : -1) + "px, 18px)"
+      );
+
+      curr_pos = limiter(win, (val) => {
+        console.log(val);
+        return val + 0.25 + rel_height;
+      });
+
+      elem.css("opacity", curr_pos);
+
+      const sp = 20;
+      elem.css(
+        "color",
+        "color-mix(in srgb, var(--final-color) " +
+          (100 * Math.pow(2, curr_pos * sp)) / Math.pow(2, sp) +
+          "%, var(--initial-color))"
+      );
+    };
+
+    let calc_win_dist = (elem) => {
+      let para_top = elem.offset().top;
+      let para_h = elem.height();
+      return (scroll - para_top) / ($(window).height() + para_h) + 1;
+    };
+
+    css_change($(".para-1"), calc_win_dist($(".para-1>.text-contain")));
+    css_change($(".para-2"), calc_win_dist($(".para-2>.text-contain")), true);
+    css_change($(".para-3"), calc_win_dist($(".para-3>.text-contain")));
+  };
+
+  // change Mask behavior
+
+  if (!$.browser.mobile) {
+    $(".scrollbar-hidden::-webkit-scrollbar").css("display", "none");
+    $(".scrollbar-hidden").css(
+      "-ms-overflow-style",
+      "none",
+      "scrollbar-width",
+      "none"
+    );
+
+    $(document).mousemove((event) => {
+      mouse_y = event.clientY - 350 / 2;
+      mouse_x = event.clientX - 350 / 2;
+      set_maskpos();
+    });
+
+    $(window).scroll((event) => {
+      scroll = $(window).scrollTop();
+      set_maskpos();
+      show_text();
+    });
+  } else {
+    $(".img").css("transition", "0.1s ease");
+    let mouse_y = $(window).height() / 2 - 350 / 2;
+    let mouse_x = $(window).width() / 2 - 350 / 2;
+
+    $(window).scroll((event) => {
+      scroll = $(window).scrollTop();
+      $(".img").css(
+        "mask-position",
+        mouse_x +
+          "px" +
+          " " +
+          ((scroll / $(window).height()) * paralax * 5 + mouse_y) +
+          "px"
+      );
+      show_text();
+    });
+  }
+
+  // inititial opener seq
+
+  let word_list = "Und Dann gab's Keines Mehr".split(" ");
+
+  let delayFn = (val) => {
+    return (Math.log(val) * 2 + 1) * (Math.log(val) * 3.5 + 1) * 100;
+  };
+
+  word_list.forEach((word, i) => {
+    $(".opener>.text-container").append(
+      "<span class='initial-state word-" + i + "'>" + word + "</span>"
+    );
+    $(".opener>.text-container").append(" ");
+  });
+
+  word_list.forEach((_, i) => {
+    setTimeout(() => {
+      $(".word-" + i).removeClass("initial-state");
+      $(".word-" + i).addClass("anim");
+    }, delayFn(i));
+  });
+
   setTimeout(() => {
-    $(".word-" + i).removeClass("initial-state");
-    $(".word-" + i).addClass("anim");
-  }, delayFn(i));
+    $(".img").addClass("imganim");
+  }, delayFn(word_list.length / 2));
+
+  setTimeout(() => {
+    $(".cont").css("opacity", "1");
+  }, delayFn(word_list.length / 2) + 2000);
+
+  setTimeout(() => {
+    $(".overlay-text").removeClass("initial-state");
+    $(".overlay-text").addClass("animate-overlay-text");
+  }, delayFn(word_list.length / 2) + 2000);
 });
-
-setTimeout(() => {
-  $(".cont").css("opacity", "1");
-}, delayFn(word_list.length + 1) + 4500);
-
-setTimeout(() => {
-  $(".overlay-text").removeClass("initial-state");
-  $(".overlay-text").addClass("animate-overlay-text");
-}, delayFn(word_list.length + 1) + 5000);
-
-let mouse_y = $(window).height() / 2 - 350 / 2;
-let mouse_x = $(window).width() / 2 - 350 / 2;
-let scroll = 0;
-let paralax = 200;
-
-function set_maskpos() {
-  $(".img").css(
-    "mask-position",
-    mouse_x + "px" + " " + (mouse_y + scroll) + "px"
-  );
-  $(".img").css(
-    "background-position",
-    ((0.5 * mouse_x) / $(window).height()) * paralax +
-      "px" +
-      " " +
-      ((0.5 * mouse_y) / $(window).width()) * paralax +
-      "px"
-  );
-}
-
-set_maskpos();
-
-let limiter = (val, fn) => {
-  return Math.min(1, Math.max(0, fn(val)));
-};
-let show_text = () => {
-  let opacity_fn = (val) => {
-    return val * val * val * 5.359375 - 0.45;
-  };
-
-  let css_change = (elem, win, invert = false) => {
-    rel_height = ($(elem).height()/$(window).height());
-    p20 = $(window).height()*0.2;
-    console.log(rel_height);
-    elem.css("opacity", limiter(win, opacity_fn));
-    const shift = 100;
-    let move_amount = -shift * (limiter(win, opacity_fn) - 1);
-    elem.css(
-      "transform",
-      "translate(" + move_amount * (!invert ? 1 : -1) + "px, 18px)"
-    );
-    elem.css("color","color-mix(in srgb, var(--final-color) "+ 100*limiter(win, (val) => {val += rel_height+p20; return Math.log2(val)+0.9;}) +"%, var(--initial-color))")
-  };
-
-  let calc_win_dist = (elem) => {
-    let para_top = elem.offset().top;
-    let para_h = elem.height();
-    return (scroll - para_top) / ($(window).height() + para_h) + 1;
-  };
-
-  css_change($(".para-1"), calc_win_dist($(".para-1>.text-contain")));
-  css_change($(".para-2"), calc_win_dist($(".para-2>.text-contain")), true);
-  css_change($(".para-3"), calc_win_dist($(".para-3>.text-contain")));
-
-  //     if () {
-  //         $(".para-1").addClass("pop-in-text-left");
-  //         $(".para-1").removeClass("initial-state");
-  //     }
-
-  //     if ($(".para-2").offset().top - $(window).height() / 2 <= scroll) {
-  //         $(".para-2").addClass("pop-in-text-right");
-  //         $(".para-2").removeClass("initial-state");
-  //     }
-
-  //     if ($(".para-3").offset().top - $(window).height() / 2 <= scroll) {
-  //         $(".para-3").addClass("pop-in-text-left");
-  //         $(".para-3").removeClass("initial-state");
-  //     }
-};
-
-if (!$.browser.mobile) {
-  $(".scrollbar-hidden::-webkit-scrollbar").css("display", "none");
-  $(".scrollbar-hidden").css(
-    "-ms-overflow-style",
-    "none",
-    "scrollbar-width",
-    "none"
-  );
-
-  $(document).mousemove((event) => {
-    mouse_y = event.clientY - 350 / 2;
-    mouse_x = event.clientX - 350 / 2;
-    set_maskpos();
-  });
-
-  $(window).scroll((event) => {
-    scroll = $(window).scrollTop();
-    set_maskpos();
-    show_text();
-  });
-} else {
-  $(".img").css("transition", "0.1s ease");
-  let mouse_y = $(window).height() / 2 - 350 / 2;
-  let mouse_x = $(window).width() / 2 - 350 / 2;
-
-  $(window).scroll((event) => {
-    scroll = $(window).scrollTop();
-    $(".img").css(
-      "mask-position",
-      mouse_x +
-        "px" +
-        " " +
-        ((scroll / $(window).height()) * paralax * 5 + mouse_y) +
-        "px"
-    );
-    show_text();
-  });
-}
